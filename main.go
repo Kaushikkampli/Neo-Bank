b71f8badfeb31d0044dccd69d6bfd451aa3a0bc0
kaushikkampli <kaushikkampli@gmail.com>
2024-04-10 00:28:39 +0530

    env vars

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/kaushikkampli/neobank/api"
 	db "github.com/kaushikkampli/neobank/db/sqlc"
+	"github.com/kaushikkampli/neobank/utils"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:postgres@localhost:3308/neo_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
 	var err error
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+
+	fmt.Println("here")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +27,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
